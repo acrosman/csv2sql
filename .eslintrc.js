71109882bb4da0479408cb8428c8c147edaf64fb
Aaron Crosman <acrosman@gmail.com>
2021-08-11 23:39:13 -0400

    Metalint and allow console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-  "env": {
-    "browser": false,
-    "es6": true,
-    "jest/globals": true,
+  env: {
+    browser: false,
+    es6: true,
+    'jest/globals': true,
   },
-  "extends": "airbnb-base",
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly",
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "plugins": ["jest"],
-  "rules": {
-    "no-unused-vars": 1,
-    "no-param-reassign": ["error", { "props": false }],
-    "no-underscore-dangle": 0,
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error",
-  }
+  plugins: ['jest'],
+  rules: {
+    'no-unused-vars': 1,
+    'no-param-reassign': ['error', { props: false }],
+    'no-underscore-dangle': 0,
+    'no-console': false,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 };
